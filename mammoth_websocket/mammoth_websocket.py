7ecf2370cf7f83b94f5d0eb0b75ffb6c196125e9
Cavon <cavonxx@gmail.com>
2025-05-23 14:52:41 +0800

    refactor(mammoth_websocket): remove send in onreceive. simplify client management and remove unused code
    Remove redundant client tracking and broadcasting logic to streamline the WebSocket implementation. The changes focus on handling a single client connection more efficiently and removing unused methods and variables to improve code maintainability.

--- mammoth_websocket/mammoth_websocket.py
+++ mammoth_websocket/mammoth_websocket.py
@@ -1,7 +1,6 @@
 import asyncio
 import websockets
 import json
-import time
 import threading
 
 class Client:
@@ -14,20 +13,17 @@ class MammothWebSocket():
 
     DEFAULT_PORT = 30102
 
-    def __init__(self, port=DEFAULT_PORT, data_interval=20):
+    def __init__(self, port=DEFAULT_PORT):
         self.port = port
-        self.client_num = 0
-        self.clients = {}
-        self.data_interval = data_interval
+        self.websocket = None
+        self.client_ip = None
         self.running = False
         self.server_thread = None
-        self.broadcast_thread = None
         self.loop = None
-        self.__user_on_device_config__ = lambda x: None
-        self.__user_on_io_data__ = lambda x: None
-        self.__user_on_connect__ = lambda x: None
-        self.__user_on_disconnect__ =lambda x: None
-        self.io_data = {}
+        self.__user_on_device_config__ = None
+        self.__user_on_io_data__ = None
+        self.__user_on_connect__ = None
+        self.__user_on_disconnect__ =None
         self.__device_info__ = {}
 
     def set_device_info(self, device_info):
@@ -45,54 +41,38 @@ class MammothWebSocket():
     def set_on_disconnect(self, on_disconnect):
         self.__user_on_disconnect__ = on_disconnect
 
-    def update_io_data(self, data):
-        if isinstance(data, dict):
-            self.io_data = {}
-            self.io_data.update(data)
-
     async def on_connect(self, websocket):
-        client_id = self.client_num
-        client_ip = websocket.remote_address[0]
-        client = Client(client_id, client_ip, websocket)
-        self.clients[client_id] = client
+        if self.websocket:
+            return False
+        self.websocket = websocket
+        self.client_ip = websocket.remote_address[0]
 
-        print(f'client {client_id, client_ip} connected')
+        print(f'client {self.client_ip} connected')
 
         # Send device info
         await websocket.send(json.dumps(self.__device_info__))
 
-        try:
-            self.__user_on_connect__(client_id)
-        except Exception as e:
-            print(f"on_connect error: {str(e)}")
-    
-        self.client_num += 1
+        if self.__user_on_connect__:
+            self.__user_on_connect__()
+        return True
 
-        return client_id
+    async def on_disconnect(self):
+        print(f'client {self.client_ip} disconnected')
+        self.client = None
 
-    async def on_disconnect(self, client_id):
-        client = self.clients[client_id]
-        print(f'client {client.id, client.ip} disconnected')
-        self.clients.pop(client.id, None)
+        if self.__user_on_disconnect__:
+            self.__user_on_disconnect__()
 
-        try:
-            self.__user_on_disconnect__(client.id)
-        except Exception as e:
-            print(f"on_disconnect error: {str(e)}")
-
-    async def on_receive(self, data, client_id):
+    async def on_receive(self, data):
         if isinstance(data, str):
-            # print(f"Received string ({client_id}): {data}")
             if data.startswith('SET+'):
                 data = data[4:]
                 try:
                     data = json.loads(data)
                 except Exception as e:
                     await self.response('ERROR', ['Invalid json format', f'{e}'] )
-                try:
-                    self.__user_on_device_config__(data)
-                except Exception as e:
-                    print(f"on_io_data error: {str(e)}")
+                if self.__user_on_device_config__:
+                    await self.__user_on_device_config__(data)
                 await self.response('OK')
             if data.startswith('DATA+'):
                 data = data[5:]
@@ -102,106 +82,53 @@ class MammothWebSocket():
                     except Exception as e:
                         await self.response('ERROR', ['Invalid json format', f'{e}'] )
 
-                # send data to client
-                # print(f"GS: {self.io_data['grayscale_value']}")
-                to_send = self.get_packed_io_data()
-                # print(f"Send data: {to_send}")
-                await self.send(to_send, client_id)
-
                 # handle received data
-                try:
-                    self.__user_on_io_data__(data)
-                except Exception as e:
-                    print(f"on_io_data error: {str(e)}")
+                if self.__user_on_io_data__:
+                    await self.__user_on_io_data__(data)
             else:
                 await self.response('ERROR', ['Invalid command format'])
 
-    async def receive_message(self, websocket, client_id):
+    async def receive_message(self, websocket):
         try:
             async for message in websocket:
-                await self.on_receive(message, client_id)
+                await self.on_receive(message)
         except websockets.exceptions.ConnectionClosedError as e:
-            print(f'Client {client_id} disconnected: {e}')
+            self.on_disconnect()
 
     async def websocket_loop(self, websocket):
-        client_id = await self.on_connect(websocket)
+        print("websocket_loop")
+        if not await self.on_connect(websocket):
+            return False
 
         try:
-            await self.receive_message(websocket, client_id)
+            await self.receive_message(websocket)
         finally:
-            await self.on_disconnect(client_id)
+            await self.on_disconnect()
 
     def start(self):
         self.running = True
         self.server_thread = threading.Thread(target=self.server_run)
         self.server_thread.daemon = True
         self.server_thread.start()
-        # self.broadcast_thread = threading.Thread(target=self.run_broadcast)
-        # self.broadcast_thread.start()
-
-    def run_broadcast(self):
-        self.broadcast_loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.broadcast_loop)
-        self.broadcast_loop.run_until_complete(self.broadcast_io_data())
-
-    def get_packed_io_data(self):
-        data = { "io_data": self.io_data }
-        data = json.dumps(data)
-
-        # Add data header
-        data = f'DATA+{data}'
-        return data
-
-    async def broadcast_io_data(self):
-        """Broadcast sensor data to all connected clients."""
-        while self.running:
-            if len(self.io_data) == 0:
-                await asyncio.sleep(self.data_interval/1000)
-                continue
-
-            if len(self.clients) == 0:
-                await asyncio.sleep(self.data_interval/1000)
-                continue
-
-            data = self.get_packed_io_data()
-
-            # 遍历所有客户端广播
-            print(f'broadcast data: {data}')
-            await self.send_all(data)
-            self.io_data = {}
-            
-            await asyncio.sleep(self.data_interval/1000)
 
     def close(self):
         self.server.close()
         self.running = False
-        while len(self.clients):
-            time.sleep(0.01)
-        print('close done')
 
     def server_run(self):
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
         self.loop.run_until_complete(self.main())
 
-    async def send_all(self, data):
-        tasks = []
-        for client_id in self.clients:
-            tasks.append(self.send(data, client_id))
-        if tasks:
-            await asyncio.gather(*tasks)
-
-    async def send(self, data, client_id):
-        client = self.clients.get(client_id)
+    async def send(self, data):
         try:
-            await client.websocket.send(data)
+            await self.websocket.send(data)
         except websockets.exceptions.ConnectionClosedError as e:
-            print(f'Client {client_id} disconnected: {e}')
-            self.on_disconnect(client_id)
+            self.on_disconnect()
         except websockets.exceptions.ConnectionClosedOK as e:
-            self.on_disconnect(client_id)
+            self.on_disconnect()
         except RuntimeError as e:
-            self.on_disconnect(client_id)
+            self.on_disconnect()
 
     async def response(self, status, error=[], data={}):
         _response = {
@@ -209,7 +136,7 @@ class MammothWebSocket():
             'error': error,
             'data': data
         }
-        await self.send_all(json.dumps(_response))
+        await self.send(json.dumps(_response))
 
     async def main(self):
         self.server = await websockets.serve(self.websocket_loop, "0.0.0.0", self.port)
